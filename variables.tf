1a852ee8f2eb41960a48c01d1bd9e39229836325
alaa <alaa@localhost.localdomain>
2023-05-11 21:51:04 -0700

    create automated infrastruture using terraform

---
+++ variables.tf
@@ -0,0 +1,8 @@
+
+variable "avail_zone" {}
+variable "vpc-cidr" {}
+variable "sub-cidr" {}
+variable "env-prefix" {}
+variable "default_route"{}
+variable "instance_type" {}
+variable "key-location" {}
