98647b23b1ffb7651b41ba8011fc82f86cc411ef
alaa <alaa@localhost.localdomain>
2023-05-13 15:20:49 -0700

    change backend from local to s3

--- providers.tf
+++ providers.tf
@@ -4,3 +4,13 @@ provider "aws" {
 region="us-east-1"
 
 }
+
+terraform {
+  required_version=">=0.12"
+  backend "s3" {
+    bucket="myapp-bucket"
+    key ="terraform.tfstate"
+    region ="us-east-1"
+  }
+
+}
